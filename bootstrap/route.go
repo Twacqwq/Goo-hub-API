61674a28d0b4a5011b68bfb673576a54058265eb
twacqwq <xiaot.qaq@gmail.com>
2022-10-10 16:30:48 +0800

    init route

---
+++ bootstrap/route.go
@@ -0,0 +1,43 @@
+package bootstrap
+
+import (
+	"net/http"
+	"strings"
+	"thub/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+// 路由注册中心
+func SetupRoute(router *gin.Engine) {
+
+	// 注册全局中间件
+	registerGlobalMiddlewarte(router)
+
+	// 注册404请求handler
+	setupNotFoundHandler(router)
+
+	// 注册API路由
+	routes.RegisterAPIRoutes(router)
+}
+
+func registerGlobalMiddlewarte(router *gin.Engine) {
+	router.Use(
+		gin.Logger(),
+		gin.Recovery(),
+	)
+}
+
+func setupNotFoundHandler(router *gin.Engine) {
+	router.NoRoute(func(c *gin.Context) {
+		acceptString := c.Request.Header.Get("accept")
+		if strings.Contains(acceptString, "text/html") {
+			c.String(http.StatusNotFound, "404 NOT FOUND")
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "404 NOT FOUND",
+			})
+		}
+	})
+}
--- main.go
+++ main.go
@@ -1,33 +1,20 @@
 package main
 
 import (
-	"net/http"
-	"strings"
+	"fmt"
+	"thub/bootstrap"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
+	router := gin.New()
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"hello": "world",
-		})
-	})
+	// 注册路由
+	bootstrap.SetupRoute(router)
 
-	// 处理404请求
-	r.NoRoute(func(c *gin.Context) {
-		acceptString := c.Request.Header.Get("Accept")
-		if strings.Contains(acceptString, "text/html") {
-			c.String(http.StatusNotFound, "404 NOT FOUND")
-		} else {
-			c.JSON(http.StatusNotFound, gin.H{
-				"error_code":    404,
-				"error_message": "路由未定义, 请确认url和请求方法是否正确。",
-			})
-		}
-	})
-
-	r.Run(":9999")
+	err := router.Run(":9999")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
