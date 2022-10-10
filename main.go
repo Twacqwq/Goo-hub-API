f5de0433a20b7a94d16fec75a6ebf880ab324acf
twacqwq <xiaot.qaq@gmail.com>
2022-10-10 16:14:39 +0800

    integration gin

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello world!!!!")
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"hello": "world",
+		})
+	})
+
+	// 处理404请求
+	r.NoRoute(func(c *gin.Context) {
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			c.String(http.StatusNotFound, "404 NOT FOUND")
+		} else {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义, 请确认url和请求方法是否正确。",
+			})
+		}
+	})
+
+	r.Run(":9999")
 }
