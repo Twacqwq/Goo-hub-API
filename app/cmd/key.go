f7bbdc5b211efefbe57d8a6186413319fe25a2cd
twacqwq <xiaot.qaq@gmail.com>
2022-10-15 14:36:39 +0800

    key command

---
+++ app/cmd/key.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"thub/pkg/console"
+	"thub/pkg/helpers"
+
+	"github.com/spf13/cobra"
+)
+
+var CmdKey = &cobra.Command{
+	Use:   "key",
+	Short: "Generate App Key, will print the generated key",
+	Run:   runKeyGenerate,
+	Args:  cobra.NoArgs, // 不允许传参
+}
+
+func init() {
+	rootCmd.AddCommand(CmdKey)
+}
+
+func runKeyGenerate(cmd *cobra.Command, args []string) {
+	console.Success("---")
+	console.Success("App Key:")
+	console.Success(helpers.RandomString(32))
+	console.Success("---")
+	console.Warning("please go to .env file to change the APP_KEY option")
+}
