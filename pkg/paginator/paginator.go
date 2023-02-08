cf1e4b7d61206c2d2cc68d1d9f9faf561ec7eab7
twacqwq <xiaot.qaq@gmail.com>
2023-02-08 20:00:31 +0800

    fix: paginator

--- pkg/paginator/paginator.go
+++ pkg/paginator/paginator.go
@@ -145,7 +145,7 @@ func (p *Paginator) formatBaseURL(baseURL string) string {
 // 拼接分页链接
 func (p Paginator) getPageLink(page int) string {
 	return fmt.Sprintf("%v%v&%s=%s&%s=%s&%s=%v",
-		p.BaseURL,
+		p.formatBaseURL(p.BaseURL),
 		page,
 		config.Get("paging.url_query_sort"),
 		p.Sort,
